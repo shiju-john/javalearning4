96d31155ca88fa416bb993f575fee9f434ffdd9f
shiju.john <shiju.john@flytxt.com>
2017-02-03 14:42:21 +0530

    Spring boot application.yaml property read

--- javalearning/springboot/src/main/java/com/javalearing4/springboot/config/ConfigurationBean.java
+++ javalearning/springboot/src/main/java/com/javalearing4/springboot/config/ConfigurationBean.java
@@ -1,7 +1,10 @@
 package com.javalearing4.springboot.config;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 /**
  * 
@@ -9,13 +12,22 @@ import org.springframework.context.annotation.Configuration;
  *
  */
 @Configuration
+@EnableConfigurationProperties
 public class ConfigurationBean {
+	
+	@Value("${server.value}")
+	public String serverValue;
+	
 	
 	@PostConstruct
 	public void postConstaructMethod(){
 		System.out.println("Post Construct Method");
 	}
 	
+	@PreDestroy
+	public void presdetroy(){
+		System.out.println("Config value "+serverValue+" destroy method");
+	}
 	
 
 }
